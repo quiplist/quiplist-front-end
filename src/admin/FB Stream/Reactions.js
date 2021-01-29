3d1745d8fd6f5b61d55454f07b9207b9c48f0c7f
Christine Lee <lee.christinedazo@gmail.com>
2021-01-29 10:23:45 +0800

    updated reactions

--- src/admin/FB Stream/Reactions.js
+++ src/admin/FB Stream/Reactions.js
@@ -10,6 +10,8 @@ class Reactions extends Component {
       activeWow: false,
       activeSad: false,
       activeAngry: false,
+      activeImg: '/img/like.png',
+      active: 'Like',
     };
 
   }
@@ -18,31 +20,39 @@ class Reactions extends Component {
   render() {
     return (
       <div className="feed"> 
-        <a className="like-btn">Like
+        <a className="like-btn"> 
+          <img src={this.state.activeImg} alt="emoji" className="active-emoji"/><h4>{this.state.active}</h4>
           <div className="reaction-box">
-              <div className={this.state.activeLike ? 'show' : 'reaction-icon'} onClick={() => this.setState({ activeLike: !this.state.activeLike })}>
-                  <img src="/img/like.png" alt="like"/>
-                  <label>Like</label>
+              <div className="reaction-icon" onClick={() => this.setState({ activeLike: !this.state.activeLike, 
+              activeImg: '/img/like.png', activeLike: !this.state.activeLike, active: 'Like' })}>
+                  {this.state.activeLike ? 
+                    <div><img src="/img/like.png" alt="like"/><label>Liked</label></div> : 
+                    <div><img src="/img/like.png" alt="like"/><label>Like</label></div> }
               </div>
-              <div className={this.state.activeHeart ? 'show' : 'reaction-icon'} onClick={() => this.setState({ activeHeart: !this.state.activeHeart })}>
-                  <img src="/img/heart.png" className="img-fluid" alt="heart"/>
-                  <label>Love</label>
+              <div className="reaction-icon" onClick={() => this.setState({ activeHeart: !this.state.activeHeart, activeImg: '/img/heart.png', active: 'Love'  })}>
+                  {this.state.activeHeart ? 
+                    <div><img src="/img/heart.png" alt="love"/><label>{this.setLove}</label></div> : 
+                    <div><img src="/img/heart.png" alt="love"/><label>Love</label></div> }
               </div>
-              <div className={this.state.activeHappy ? 'show' : 'reaction-icon'} onClick={() => this.setState({ activeHappy: !this.state.activeHappy })}>
-                  <img src="/img/happy.png" className="img-fluid" alt="happy"/>
-                  <label>Haha</label>
+              <div className="reaction-icon" onClick={() => this.setState({ activeHappy: !this.state.activeHappy, activeImg: '/img/happy.png' , active: 'Happy'})}>
+                {this.state.activeHappy ? 
+                <div><img src="/img/happy.png" alt="happy"/><label>Haha</label></div> : 
+                <div><img src="/img/happy.png" alt="happy"/><label>Haha</label></div>}
               </div>
-              <div className={this.state.activeWow ? 'show' : 'reaction-icon'} onClick={() => this.setState({ activeWow: !this.state.activeWow })}>
-                  <img src="/img/wow.png" className="img-fluid" alt="wow"/>
-                  <label>Wow</label>
+              <div className="reaction-icon" onClick={() => this.setState({ activeWow: !this.state.activeWow, activeImg: '/img/wow.png', active: 'Wow'})}>
+                  {this.state.activeWow ? 
+                  <div><img src="/img/wow.png" alt="wow"/><label>Wowed</label></div> : 
+                  <div><img src="/img/wow.png" alt="wow"/><label>Wow</label></div>}
               </div>
-              <div className={this.state.activeSad ? 'show' : 'reaction-icon'} onClick={() => this.setState({ activeSad: !this.state.activeSad })}>
-                  <img src="/img/sad.png" className="img-fluid" alt="sad"/>
-                  <label>Sad</label>
+              <div className="reaction-icon" onClick={() => this.setState({ activeSad: !this.state.activeSad, activeImg: '/img/sad.png', active: 'Sad'})}>
+                  {this.state.activeSad ? 
+                  <div><img src="/img/sad.png" alt="sad"/><label>Sad</label></div> : 
+                  <div><img src="/img/sad.png" alt="sad"/><label>Sad</label></div>}
               </div>
-              <div className={this.state.activeAngry ? 'show' : 'reaction-icon'} onClick={() => this.setState({activeAngry: !this.state.activeAngry })}>
-                  <img src="/img/angry.png" className="img-fluid" alt="angry"/>
-                  <label>Angry</label>
+              <div className="reaction-icon" onClick={() => this.setState({activeAngry: !this.state.activeAngry, activeImg: '/img/angry.png', active: 'Angry' })}>
+                  {this.state.activeAngry ? 
+                  <div><img src="/img/angry.png" alt="angry"/><label>Angry</label></div> : 
+                  <div><img src="/img/angry.png" alt="angry"/><label>Angry</label></div>}
               </div>
           </div> 
         </a> 
