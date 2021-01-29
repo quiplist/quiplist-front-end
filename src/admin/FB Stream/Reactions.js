2737c340f4d99faeb22ff67b9d4c1f4805f8eba8
Christine Lee <lee.christinedazo@gmail.com>
2021-01-30 00:13:35 +0800

    Final reactions

--- src/admin/FB Stream/Reactions.js
+++ src/admin/FB Stream/Reactions.js
@@ -9,50 +9,111 @@ class Reactions extends Component {
       activeHappy: false,
       activeWow: false,
       activeSad: false,
-      activeAngry: false,
-      activeImg: '/img/like.png',
-      active: 'Like',
+      activeAngry: false
     };
-
+    this.setLikeIcon = this.setLikeIcon.bind(this);
+    this.setLoveIcon = this.setLoveIcon.bind(this);
+    this.setHahaIcon = this.setHahaIcon.bind(this);
+    this.setWowIcon = this.setWowIcon.bind(this);
+    this.setSadIcon = this.setSadIcon.bind(this);
+    this.setAngryIcon = this.setAngryIcon.bind(this);
   }
 
+  setLikeIcon = () => {
+    this.setState({
+      activeLike: !this.state.activeLike,
+    })
+    var image = document.getElementById("activeEmoji");
+    if(this.state.activeLike){
+      image.src ="/img/active-like.png";
+    }else{
+      image.src ="/img/default-icon.png";
+    }
+  }
+
+  setLoveIcon = () => {
+    this.setState({
+      activeHeart: !this.state.activeHeart,
+    })
+    var image = document.getElementById("activeEmoji");
+    if(this.state.activeHeart){
+      image.src ="/img/active-heart.png";
+    }else{
+      image.src ="/img/default-icon.png";
+    }
+  }
+
+  setHahaIcon = () => {
+    this.setState({
+      activeHappy: !this.state.activeHappy,
+    })
+    var image = document.getElementById("activeEmoji");
+    if(this.state.activeHappy){
+      image.src ="/img/active-laugh.png";
+    }else{
+      image.src ="/img/default-icon.png";
+    }
+  }
+
+  setWowIcon = () => {
+    this.setState({
+      activeSad: !this.state.activeSad,
+    })
+    var image = document.getElementById("activeEmoji");
+    if(this.state.activeSad){
+      image.src ="/img/active-wow.png";
+    }else{
+      image.src ="/img/default-icon.png";
+    }
+  }
+
+  setSadIcon = () => {
+    this.setState({
+      activeWow: !this.state.activeWow,
+    })
+    var image = document.getElementById("activeEmoji");
+    if(this.state.activeWow){
+      image.src ="/img/active-wow.png";
+    }else{
+      image.src ="/img/default-icon.png";
+    }
+  }
+
+  setAngryIcon = () => {
+    this.setState({
+      activeAngry: !this.state.activeAngry,
+    })
+    var image = document.getElementById("activeEmoji");
+    if(this.state.activeAngry){
+      image.src ="/img/active-angry.png";
+    }else{
+      image.src ="/img/default-icon.png";
+    }
+  }
 
   render() {
     return (
       <div className="feed"> 
         <a className="like-btn"> 
-          <img src={this.state.activeImg} alt="emoji" className="active-emoji"/><h4>{this.state.active}</h4>
+          <img src="/img/default-icon.png" alt="emoji" className="active-emoji" id="activeEmoji"/>
           <div className="reaction-box">
-              <div className="reaction-icon" onClick={() => this.setState({ activeLike: !this.state.activeLike, 
-              activeImg: '/img/like.png', activeLike: !this.state.activeLike, active: 'Like' })}>
-                  {this.state.activeLike ? 
-                    <div><img src="/img/like.png" alt="like"/><label>Liked</label></div> : 
-                    <div><img src="/img/like.png" alt="like"/><label>Like</label></div> }
+              <div className="reaction-icon" onClick={this.setLikeIcon}>
+                <img src="/img/active-like.png" alt="like"/><label>Like</label>
               </div>
-              <div className="reaction-icon" onClick={() => this.setState({ activeHeart: !this.state.activeHeart, activeImg: '/img/heart.png', active: 'Love'  })}>
-                  {this.state.activeHeart ? 
-                    <div><img src="/img/heart.png" alt="love"/><label>{this.setLove}</label></div> : 
-                    <div><img src="/img/heart.png" alt="love"/><label>Love</label></div> }
+              <div className="reaction-icon" onClick={this.setLoveIcon}>
+                <img src="/img/active-heart.png" alt="Love"/><label>Love</label>
               </div>
-              <div className="reaction-icon" onClick={() => this.setState({ activeHappy: !this.state.activeHappy, activeImg: '/img/happy.png' , active: 'Happy'})}>
-                {this.state.activeHappy ? 
-                <div><img src="/img/happy.png" alt="happy"/><label>Haha</label></div> : 
-                <div><img src="/img/happy.png" alt="happy"/><label>Haha</label></div>}
+              <div className="reaction-icon" onClick={this.setHahaIcon}>
+                <img src="/img/active-laugh.png" alt="Haha"/><label>Haha</label>
               </div>
-              <div className="reaction-icon" onClick={() => this.setState({ activeWow: !this.state.activeWow, activeImg: '/img/wow.png', active: 'Wow'})}>
-                  {this.state.activeWow ? 
-                  <div><img src="/img/wow.png" alt="wow"/><label>Wowed</label></div> : 
-                  <div><img src="/img/wow.png" alt="wow"/><label>Wow</label></div>}
+              <div className="reaction-icon" onClick={this.setWowIcon}>
+                <img src="/img/active-wow.png" alt="Wow"/><label>Wow</label>
               </div>
-              <div className="reaction-icon" onClick={() => this.setState({ activeSad: !this.state.activeSad, activeImg: '/img/sad.png', active: 'Sad'})}>
-                  {this.state.activeSad ? 
-                  <div><img src="/img/sad.png" alt="sad"/><label>Sad</label></div> : 
-                  <div><img src="/img/sad.png" alt="sad"/><label>Sad</label></div>}
+              <div className="reaction-icon" onClick={this.setSadIcon}>
+                <img src="/img/active-sad.png" alt="Sad"/><label>Sad</label>
               </div>
-              <div className="reaction-icon" onClick={() => this.setState({activeAngry: !this.state.activeAngry, activeImg: '/img/angry.png', active: 'Angry' })}>
-                  {this.state.activeAngry ? 
-                  <div><img src="/img/angry.png" alt="angry"/><label>Angry</label></div> : 
-                  <div><img src="/img/angry.png" alt="angry"/><label>Angry</label></div>}
+              <div className="reaction-icon" onClick={this.setAngryIcon}>
+                <img src="/img/active-angry.png" alt="Angry"/><label>Angry</label>
               </div>
           </div> 
         </a> 
